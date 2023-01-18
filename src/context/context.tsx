45c2e0a12b111e5bce7fcb60ad4b53e1abe35168
darwanceyhan <darvan111@hotmail.com>
2023-01-18 15:57:15 +0300

    add axios and create getStrinData method in context.tsx

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,5 +1,35 @@
-import {createContext} from 'react';
+import axios from "axios";
+import React, { createContext } from "react";
 
-export const Context = createContext();
+export const Context = createContext({
+  Data: [],
+  GetStringData: () => {},
+});
 
-export class 
+export class SpeedAppContext extends React.Component<{
+  childern: React.ReactNode;
+}> {
+  state = {
+    Data: Array<string>,
+    GetStringData: () => {},
+  };
+  GetStringData() {
+    axios
+      .get("https://random-word-api.herokuapp.com/word?number=12")
+      .then((res) => {
+        this.setState({ Data: res.data });
+      })
+      .then(() => {
+        console.log(this.state.Data);
+      });
+  }
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+export default SpeedAppContext;
